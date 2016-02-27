76623ea605acae0c18f79e46c16cd2f944c2e9cd
Keith Yong <terda12@gmail.com>
2016-02-26 19:35:12 -0500

    Implemented request_for_token function
    Added some some comments to describe the functions

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import requests
 import time
 import json
 import base64
+import config
 
+# encode_consumer_key_and_secret
+# -----------------
 # Returns encrypted consumer key/secret combination as specified by this documentation:
 # https://dev.twitter.com/oauth/application-only
 def encode_consumer_key_and_secret(consumer_key="", consumer_secret=""):
@@ -11,10 +14,22 @@ def encode_consumer_key_and_secret(consumer_key="", consumer_secret=""):
 
     return base64.b64encode(full_key).decode("utf-8")
 
-# Request for application only token key and secret
+# request_for_token
+# -----------------
+# Request for application only token key and secret as specified by this documentation:
+# https://dev.twitter.com/oauth/application-only
 def request_for_token(consumer_key="", consumer_secret=""):
-    # TODO
-    return 0
+    url = "https://api.twitter.com/oauth2/token"
+    encoded_key_secret_combo = encode_consumer_key_and_secret(config.consumer_key, config.consumer_secret)
+
+    headers = {
+        'Authorization': "Basic " + encoded_key_secret_combo,
+        'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8"
+    }
+
+    r = requests.post(url, data="grant_type=client_credentials", headers=headers)
+
+    return r.json()['access_token']
 
 def search_by_keyword(keyword=""):
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
@@ -34,4 +49,4 @@ def search_by_keyword(keyword=""):
         print(tweet['text'])
         print('-------------')
 
-print(encode_consumer_key_and_secret(consumer_key, consumer_secret))
+print(request_for_token(config.consumer_key, config.consumer_secret))
